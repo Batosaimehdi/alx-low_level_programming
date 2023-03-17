c1b9cf5164b1de2aff07798aa81eed63ee0f6a3d
Batosaimehdi <sebbarmehdi77@gmail.com>
2023-03-17 03:36:08 -0700

    llkjhgfddgt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,25 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (sucess)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
-
-
-5-printf.c
-
-#include<stdio.h>
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle")
+		returne (0);
 }
